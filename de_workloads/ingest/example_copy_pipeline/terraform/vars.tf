a4d858ddb4c9819e0dc5707fedb8096a55871ef5
George Calvert <george.calvert@amido.com>
2025-05-22 16:25:31 +0100

    PR comment

--- de_workloads/ingest/example_copy_pipeline/terraform/vars.tf
+++ de_workloads/ingest/example_copy_pipeline/terraform/vars.tf
@@ -1,30 +1,30 @@
 variable "processing_workspace_id" {
   type        = string
-  default     = ""
+  default     = "8794a888-b6c6-4fe5-8f69-6eb0467f5246"
   description = "The processing workspace ID for the data pipeline."
 }
 
 variable "bronze_workspace_id" {
   type        = string
-  default     = ""
+  default     = "168a5923-e2bc-4ba8-a291-e9b77b93598c"
   description = "The bronze workspace ID"
 }
 
 variable "bronze_lakehouse_id" {
   type        = string
-  default     = ""
+  default     = "598c73d0-3dc4-4832-8391-be168728c63f"
   description = "The bronze lakehouse Id"
 }
 
 variable "silver_workspace_id" {
   type        = string
-  default     = ""
+  default     = "168a5923-e2bc-4ba8-a291-e9b77b93598c"
   description = "The silver workspace ID"
 }
 
 variable "silver_lakehouse_id" {
   type = string
-  default = ""
+  default = "08d36aeb-0278-4700-b2fa-dbaac339793c"
   description = "The silver lakehouse id"
 }
 
