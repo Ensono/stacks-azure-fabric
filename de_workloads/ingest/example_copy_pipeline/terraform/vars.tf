1b46197e9f594fb6f70b4cd251c57dc0687c9e08
George Calvert <george.calvert@amido.com>
2025-05-23 10:42:40 +0100

    remove faric ids

--- de_workloads/ingest/example_copy_pipeline/terraform/vars.tf
+++ de_workloads/ingest/example_copy_pipeline/terraform/vars.tf
@@ -1,30 +1,30 @@
 variable "processing_workspace_id" {
   type        = string
-  default     = "8794a888-b6c6-4fe5-8f69-6eb0467f5246"
+  default     = ""
   description = "The processing workspace ID for the data pipeline."
 }
 
 variable "bronze_workspace_id" {
   type        = string
-  default     = "168a5923-e2bc-4ba8-a291-e9b77b93598c"
+  default     = ""
   description = "The bronze workspace ID"
 }
 
 variable "bronze_lakehouse_id" {
   type        = string
-  default     = "598c73d0-3dc4-4832-8391-be168728c63f"
+  default     = ""
   description = "The bronze lakehouse Id"
 }
 
 variable "silver_workspace_id" {
   type        = string
-  default     = "168a5923-e2bc-4ba8-a291-e9b77b93598c"
+  default     = ""
   description = "The silver workspace ID"
 }
 
 variable "silver_lakehouse_id" {
   type = string
-  default = "08d36aeb-0278-4700-b2fa-dbaac339793c"
+  default = ""
   description = "The silver lakehouse id"
 }
 
