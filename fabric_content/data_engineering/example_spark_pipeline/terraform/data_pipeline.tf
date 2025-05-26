8fbca1e03085e813f861e3a81a3e1a0c78d3f695
Paulina Motyl <paulina.motyl@ensono.com>
2025-05-26 11:55:11 +0200

    cleanup

--- fabric_content/data_engineering/example_spark_pipeline/terraform/data_pipeline.tf
+++ fabric_content/data_engineering/example_spark_pipeline/terraform/data_pipeline.tf
@@ -21,14 +21,14 @@ resource "fabric_spark_job_definition" "example_spark_job" {
 resource "fabric_data_pipeline" "example_spark_pipeline" {
   display_name = "example_spark_pipeline"
   description  = "Example data pipeline with a Spark job"
-  workspace_id = var.processing_workspace_id
+  workspace_id = var.engineering_workspace_id
   format       = "Default"
   definition = {
     "pipeline-content.json" = {
       source = "${path.module}/../definition/example_spark_pipeline.json.tmpl"
       tokens = {
         "sparkJobDefinitionId" = fabric_spark_job_definition.example_spark_job.id
-        "processingWorkspaceId" = var.processing_workspace_id
+        "engineeringWorkspaceId" = var.engineering_workspace_id
       }
     }
   }
