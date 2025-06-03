8f5c162bc211f1f5f8bd708e7e4e47de19312443
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2025-06-03 09:07:07 +0100

    Combined providers into one file ***NO_CI***
    Signed by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- fabric_content/data_engineering/example_copy_pipeline/terraform/providers.tf
+++ fabric_content/data_engineering/example_copy_pipeline/terraform/providers.tf
@@ -1,9 +1,16 @@
 terraform {
 
+  required_version = ">= 1.8, < 2.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
+    }
+
+    fabric = {
+      source  = "microsoft/fabric"
+      version = "1.1.0"
     }
   }
 
