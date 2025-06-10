06d77f7db80d4d526c0583bfdcb6937176ea6704
Jose Carrera <jose.carrera@amido.com>
2025-06-10 09:58:28 +0100

    wip session working, still to fix assertion

--- fabric_content/data_engineering/example_spark_pipeline/spark_job/tests/component/steps/test_transform_and_save.py
+++ fabric_content/data_engineering/example_spark_pipeline/spark_job/tests/component/steps/test_transform_and_save.py
@@ -6,20 +6,21 @@ import pytest
 from pyspark.sql import SparkSession
 from data_engineering.example_spark_pipeline.spark_job.example_spark_job import transform_and_save
 from pytest_bdd import scenarios, given, when, then, parsers
+import chispa
 
 scenarios("../features/transform_and_save.feature")
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def spark():
     spark = (
         SparkSession.builder
-        .master("local[1]")
-        .appName("pytest-spark")
+        .appName("Testing PySpark Example")
         .config("spark.driver.bindAddress", "127.0.0.1")
         .config("spark.driver.host", "127.0.0.1")
-        .config("spark.jars.packages", "io.delta:delta-core_2.12:3.1.0")
+        .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.1.0")
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+        .config("spark.jars.repositories", "https://repo1.maven.org/maven2,https://mvnrepository.com/artifact")
         .getOrCreate()
     )
     yield spark
@@ -48,7 +49,13 @@ def run_transform(spark, temp_delta_dirs):
 
 @then("the target delta table should contain the correct aggregated data")
 def check_aggregated(spark, temp_delta_dirs):
+    import pandas as pd
+    from chispa.dataframe_comparer import assert_df_equality
     _, target_dir = temp_delta_dirs
     df = spark.read.format("delta").load(target_dir)
-    result = {row['name']: row['max_age'] for row in df.collect()}
-    assert result == {"Alice": 25, "Bob": 30, "Charlie": 35, "David": 40}
+    expected_data = pd.DataFrame({
+        "name": ["Alice", "Bob", "Charlie", "David"],
+        "max_age": [25, 30, 35, 40]
+    })
+    expected_df = spark.createDataFrame(expected_data)
+    assert_df_equality(df.orderBy("name"), expected_df.orderBy("name"), ignore_nullable=True)
