ef760946207ec9df30d26561b5e4ae16ef0f3dd9
Mehdi Kimakhe <mehdi.kimakhe@ensono.com>
2025-07-24 18:55:44 +0100

    Update Terraform configuration to use 'contains' for workspace filtering and enable preview for fabric provider

--- deploy/terraform/environment.tf
+++ deploy/terraform/environment.tf
@@ -2,7 +2,7 @@ resource "fabric_environment" "ws_envs" {
   # create a workspace for each of the environments and types, excluding storage workspaces
   for_each = toset([
     for ws in local.workspaces : ws
-    if !startswith(split("-", ws)[1], "storage")
+    if !contains(split("-", ws)[1], "storage")
   ])
 
   display_name = "${module.naming.extended_names[var.project].fabric_workspace.name}-${each.key}"
@@ -14,7 +14,7 @@ resource "fabric_spark_environment_settings" "env_spark_settings" {
   # create Spark environment settings for each environment
   for_each = toset([
     for ws in local.workspaces : ws
-    if !startswith(split("-", ws)[1], "storage")
+    if !contains(split("-", ws)[1], "storage")
   ])
 
   environment_id     = fabric_environment.ws_envs[each.key].id
